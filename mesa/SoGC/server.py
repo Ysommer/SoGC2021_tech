67a87b3839e7e4b562753b155a8fac1f478be4b4
Ysommer <yon.sommer@gmail.com>
2020-11-30 20:37:00 +0200

    can now enter empty solution path for display of initial configuration with no simulation

--- mesa/SoGC/server.py
+++ mesa/SoGC/server.py
@@ -6,16 +6,16 @@ from .model import SoGC
 
 
 # Make a world that is 50x50, on a 250x250 display.
-canvas_element = CanvasGrid(portrayCell, 20, 20, 500, 500)
+canvas_element = CanvasGrid(portrayCell, 30, 30, 1500, 1500)
 
 #inputs
 paths = [input("Enter Jason Path: \n"), input("Enter Sol Path: \n")]
 
 tracked = []
-t = int(input("Track id: "))
-while t != -1:
-    tracked.append(t)
-    t = int(input("Track id: "))
+t = input("Track id: ")
+while t != '':
+    tracked.append(int(t))
+    t = input("Track id: ")
 
 server = ModularServer(
     SoGC, [canvas_element], "S10ppy J035", {"height": 50, "width": 50, "paths": paths , "tracked": tracked}
