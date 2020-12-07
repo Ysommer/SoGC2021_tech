f4b3c4ccd59aada932c56209fb38fe028096a1a1
Ysommer <yon.sommer@gmail.com>
2020-12-07 20:49:34 +0200

    algo & bfs

--- controlCenter/algos/initAlgo.py
+++ controlCenter/algos/initAlgo.py
@@ -4,4 +4,4 @@ from ..infrastructure.grid import Grid
 
 class InitAlgo(Algo):
 
-    def __init__(self, instance_name: str, grid: Grid, robots: list, targets: list, max_makespan: int = None, max_sum: int = None, preprocess=None)::
+    def __init__(self, instance_name: str, grid: Grid, robots: list, targets: list, max_makespan: int = None, max_sum: int = None, preprocess=None):
