a36ca63c91eeb44ad73210bbbdc0ccf8b45fe0a8
Ysommer <yon.sommer@gmail.com>
2020-12-07 18:06:38 +0200

    algo & bfs

--- controlCenter/defines.py
+++ controlCenter/defines.py
@@ -14,3 +14,11 @@ class EnterCellResult(Enum):
     OBSTACLE = 1
     ROBOT = 2
     TAIL = 3
+
+
+class SolutionResult(Enum):
+    SUCCESS = 0
+    EXCEEDED_MAX_MAKESPAN = 1
+    EXCEEDED_MAX_SUM = 2
+    INTERRUPTED = 3
+
