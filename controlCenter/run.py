637b00b7e11c283199b4e2b763f091f3622d72ff
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2020-12-17 21:22:53 +0200

    analyzed_data

--- controlCenter/run.py
+++ controlCenter/run.py
@@ -1,21 +1,29 @@
 from controlCenter import ControlCenter
 from solution.solution import *
 from Utils.loadInstances import *
-
+from cgshop2021_pyutils import Instance
+import json
 
 def main():
-    instances_id = [i for i in range(161, 170)]
+    instances_id = [i for i in range(161, 180)]
     instances = load_all_instances()
-    analyzed = []
-
+    f = open("analyzed_data.json", "r")
+    analyzed_data = json.load(f)
+    f.close()
     for id in instances_id:
         instance = instances[id]
         print("Start instance: ", instance.name, "(number:"+str(id)+")")
         out_path = "../solutions/" + instance.name + "/"
-        controlCenter = ControlCenter(instance, out_path, 50000, 500000)
-        analyzed.append(controlCenter.run_all())
+        num_of_robots = instance.number_of_robots
+        max_makespan = 50 * num_of_robots
+        max_sum = 5 * max_makespan
 
-    print(analyzed)
+        controlCenter = ControlCenter(instance, out_path, max_makespan, num_of_robots)
+        analyzed_data[id] = controlCenter.run_all()
+
+    f = open("analyzed_data.json", "w")
+    json.dump(analyzed_data, f)
+    f.close()
 
 if __name__ == "__main__":
     main()
