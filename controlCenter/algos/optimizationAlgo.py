b3acbd1042e4409702df4b80b8deacd89849fbab
Ysommer <yon.sommer@gmail.com>
2020-12-27 22:30:29 +0200

    bfs

--- controlCenter/algos/optimizationAlgo.py
+++ controlCenter/algos/optimizationAlgo.py
@@ -1,7 +1,8 @@
 from solution.solution import Solution
 from copy import deepcopy
+import abc
 
-class OptimizationAlgo:
+class OptimizationAlgo(abc.ABC):
     def __init__(self, instance_name: str, solution: Solution, robots: list,
                  targets: list, obstacles: list, preprocess=None, name="", print_info=True):
         self.instance_name = instance_name
@@ -14,6 +15,12 @@ class OptimizationAlgo:
         self.preprocess = preprocess
         self.name = name
         self.print_info = print_info
+        self.solution.out["extra"] = {}
+
+    @abc.abstractmethod
+    def run(self):
+        ...
+
 
 
 
--- controlCenter/algos/optimization_algos/BFS_in_time.py
+++ controlCenter/algos/optimization_algos/BFS_in_time.py
@@ -1,19 +1,45 @@
 from algos.optimizationAlgo import OptimizationAlgo
 from infrastructure.solGrid import SolGrid
 from dataCollection.Generator import *
+from defines import *
+
+"""
+BIT run modes:
+    1 : improve last stepping robots
+    2 : 
+"""
 
 class BFS_in_time(OptimizationAlgo):
-    def __init__(self, instance_name: str, solution: Solution, robots: list,
-                 targets: list, obstacles: list, num_to_improve: int, preprocess=None, name="", print_info=True):
-        super().__init__(instance_name, solution, robots, targets, preprocess, "_BIT" + name, print_info)
+    def __init__(self, instance_name: str,
+                 solution: Solution,
+                 robots: list,
+                 targets: list,
+                 obstacles: list,
+                 num_to_improve: int,
+                 preprocess=None,
+                 name="",
+                 print_info=True):
+        super().__init__(instance_name,
+                         solution,
+                         robots,
+                         targets,
+                         preprocess,
+                         "_BIT" + name,
+                         print_info)
         self.sol_grid = SolGrid(self.robots, self.obs, solution)
         self.boundaries = {"N": self.sol_grid.max_y,
                            "S": self.sol_grid.min_y,
                            "W": self.sol_grid.min_x,
                            "E": self.sol_grid.max_x}
         self.num_to_improve = num_to_improve
-        self.solution.out["old_makespan"] = solution.out["makespan"]
-        self.solution.out["old_sum"] = solution.out["sum"]
+        if self.solution.out["algo_name"].find("BIT_BIT") != -1:
+            self.solution.out["algo_name"] = self.solution.out["algo_name"][:(-1)*len("_BIT")]
+            self.solution.out["extra"]["BIT_runs"] += 1
+        else:
+            self.solution.out["extra"]["BIT_runs"] = 1
+        self.solution.out["extra"]["old_makespan"] = solution.out["makespan"]
+        self.solution.out["extra"]["old_sum"] = solution.out["sum"]
+        self.solution.out["extra"]["improved"] = ""
         self.sum = solution.out["sum"]
 
     def calc_new_path(self, robot_id):
@@ -49,11 +75,14 @@ class BFS_in_time(OptimizationAlgo):
             if len(to_improve) == 0:
                 to_improve = self.sol_grid.get_last_moving_robots()
             next_robot_id = to_improve.pop()
+            if next_robot_id in improved:
+                break
             new_path = self.calc_new_path(next_robot_id)
-            assert path is not None # should at worst find old path
+            assert path is not None, "BIT: a_star failed to find legal path" # should at worst find old path
             self.sol_grid.update_robot_path(next_robot_id, new_path)
             self.update_solution(next_robot_id, new_path)
 
-        self.solution.out["improved"] = improved
-        self.solution.out["makespan"] = len(self.solution.out["steps"])
+        self.solution.out["extra"]["improved"] += str(improved) + ","
+        self.solution.put_result(SolutionResult.SUCCESS, len(self.solution.out["steps"]), self.sum)
+        return self.solution
 
