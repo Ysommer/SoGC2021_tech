3ebd98982f317100fe33272a546d319ef21b85de
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-01-04 22:32:58 +0200

    WishList

---
+++ controlCenter/WishList.py
@@ -0,0 +1,19 @@
+
+
+class WishList:
+    def __init__(self, solutionsToOptimizePath: str):
+        self.solutionsToOptimizePath = solutionsToOptimizePath
+
+    def optimize_solutions(self, number_of_iterations: int = 1):
+        pass
+
+    def all_to_all(self, number_of_iterations: int = 1):
+        """
+        run all init algorithms
+        then run number_of_iterations of optimizations
+        :return:
+        """
+        pass
+
+    def zip_and_verify(self):
+        pass
