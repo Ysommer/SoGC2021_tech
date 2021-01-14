c3900e49418450850b916678b071dff191500182
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-01-14 17:39:56 +0200

    Validator completed

--- controlCenter/run-server.py
+++ controlCenter/run-server.py
@@ -4,7 +4,7 @@ sys.path.append("/home/gilbe/workspace/SoGC2021_tech")
 from WishList import WishList, WishListPackagesTypes
 
 
-packageType = WishListPackagesTypes(sys.argv[1])
+packageType = WishListPackagesTypes(int(sys.argv[1]))
 server_id = int(sys.argv[2])
 
 WishList.farm_instances(packageType, server_id)
