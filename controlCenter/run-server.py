e7709b9d18680ae56a854125c2e67d683c7f8a04
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-01-14 23:28:33 +0200

    11 servers

--- controlCenter/run-server.py
+++ controlCenter/run-server.py
@@ -9,6 +9,6 @@ packageType = WishListPackagesTypes(int(sys.argv[1]))
 
 server_id = int(sys.argv[2])
 
-WishList.farm_instances(packageType, server_id, 10)
+WishList.farm_instances(packageType, server_id, 11)
 
 compress_best_and_send(packageType.name)
