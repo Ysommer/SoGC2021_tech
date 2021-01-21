a4c8395c7bce439f5669588cbbceaf033e8dfad2
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-01-21 22:29:33 +0200

    11 servers

--- controlCenter/farm-instance.py
+++ controlCenter/farm-instance.py
@@ -4,4 +4,7 @@ sys.path.append("/home/gilbe/workspace/SoGC2021_tech")
 
 from WishList import WishList
 
-WishList.farm_instance(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+if len(sys.argv) <= 4:
+    WishList.farm_instance(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+else:
+    WishList.farm_instance(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
