3307052447ed192e86af48db4759f2af62124e95
Ysommer <yon.sommer@gmail.com>
2021-02-05 16:16:55 +0200

    bfs

--- controlCenter/algos/optimization_algos/IterSum.py
+++ controlCenter/algos/optimization_algos/IterSum.py
@@ -45,7 +45,7 @@ class IterSum(OptimizationAlgo):
         while self.solution.out["algo_name"].find("IterSum_IterSum") != -1:
             self.solution.out["algo_name"] = self.solution.out["algo_name"][:(-1) * len("_IterSum_")]
 
-        while self.solution.out["algo_name"].find("IterSum_IterSum_") != -1:
+        while self.solution.out["algo_name"].find("IterSum_") != -1:
             self.solution.out["algo_name"] = self.solution.out["algo_name"][:(-1) * len("_IterSum_")]
 
         if self.solution.out["extra"].get("IterSum_runs", None) is not None:
