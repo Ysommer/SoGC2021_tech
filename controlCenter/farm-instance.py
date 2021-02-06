73b018d661c94bfbcde688560b660c6e162c633c
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-02-06 21:00:24 +0200

    Chill

--- controlCenter/farm-instance.py
+++ controlCenter/farm-instance.py
@@ -4,6 +4,14 @@ sys.path.append("/home/gilbe/workspace/SoGC2021_tech")
 
 from WishList import WishList
 
+"""
+    Variables:
+    1: instance_id
+    2: number_of_processors - send 1
+    3: number_of_inits_per_processor
+    4: first_algo
+"""
+
 if len(sys.argv) <= 4:
     WishList.farm_instance(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 else:
